cdc53fd45c5cb0ed33ce881dfa79b95891418788
Piirainen Vanamo K <vseppane@da3-cs-dk110-13.pc.helsinki.fi>
2018-03-12 15:21:46 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const recipesRouter = require('./controllers/recipes')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.tokenExtractor)
+app.use('/api/recipes', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
+})
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
